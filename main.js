706733375e5dd1f84f7dc394541a703477f749e6
maikuraki <1530850503@qq.com>
2018-03-13 23:49:49 +0800

    '更新'

---
+++ main.js
@@ -0,0 +1,48 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({
+        frame: false,
+        width: 320,
+        height: 500
+    });
+
+    // 然后加载应用的 index.html。
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'app.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.on('closed', () => {
+        win = null;
+    });
+
+    win.on('ready-to-show',function() {
+        // electronVibrancy.SetVibrancy(win, 8);
+        win.show();
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', () => {
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (win === null) {
+        createWindow()
+    }
+});
